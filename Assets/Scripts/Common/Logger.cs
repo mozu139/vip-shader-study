402263fea6bdb05d9b3de5cab23062723093d5da
mozu139 <aaa@bbb.com>
2023-04-16 12:10:41 +0900

    VertexInfoシーン追加

---
+++ Assets/Scripts/Common/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace VipShaderStudy.Common
+{
+    public class Logger
+    {
+
+        public static void Log<T>(IEnumerable<T> v, string head = "")
+        {
+            Debug.Log($"<color=red>{head}</color>");
+            foreach(var e in v)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VertexInfo/VertexInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using VipShaderStudy;
+
+namespace VipShaderStudy.VertexInfo
+{
+    public class VertexInfo : MonoBehaviour
+    {
+        [SerializeField] MeshFilter mf;
+
+        void Start()
+        {
+            // メッシュの各頂点位置を表示
+            var positionList = mf.mesh.vertices;
+            Common.Logger.Log(positionList, "頂点位置");
+
+            // メッシュの各頂点法線を表示
+            var normalList = mf.mesh.normals;
+            Common.Logger.Log(normalList, "頂点法線");
+
+            // メッシュの各頂点UVを表示
+            var uvList = mf.mesh.uv;
+            Common.Logger.Log(uvList, "頂点UV");
+
+            // メッシュ内の三角形を構成する頂点の番号を表示
+            var triangles = mf.mesh.triangles;
+            Common.Logger.Log(triangles, "三角形を構成する頂点の番号");
+        }
+    }
+}
